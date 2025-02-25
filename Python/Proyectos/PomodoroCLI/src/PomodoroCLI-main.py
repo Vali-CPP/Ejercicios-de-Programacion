e597c90dfd00ff40269d49d75c8cbb1b0507cec0
Valiant Molero <valiantmolero@gmail.com>
2025-02-25 17:05:59 -0500

    Ya tenemos listo el temporizador, el que muestre los diferentes ciclos con sus respectivos tiempos, hay que terminar de pulir el audio que se reproduce al final de cada pomodoro, teniendo en mente que se puede cambiar por un simple mensaje que diga que se acabo el tiempo y ya el siguiente paso seria agregarle una GUI, por asi decirlo, pero eso se puede hacer despues, hay que invertir mas tiempo en investigar como podriamos reproducir el audio porque lo necesitaremos para el reproductor de ruido blanco

--- Python/Proyectos/PomodoroCLI/src/PomodoroCLI-main.py
+++ Python/Proyectos/PomodoroCLI/src/PomodoroCLI-main.py
@@ -1,5 +1,7 @@
 import time
+import sys
 import os
+import subprocess
 
 def limpiar_pantalla():
     if os.name == "nt":
@@ -23,43 +25,38 @@ def bienvenida():
     limpiar_pantalla()
     return validacion_int("Bienvenido a tu aplicacion de Pomodoro, dime que hacer: \n1)Iniciar un pomodoro\n2)Salir\n:", "Numero invalido", 0, 2)
 
-def tiempo_a_mostrar(tiempo_actual_list, tiempo_meta_list):
-    hr = int(tiempo_meta_list[0]) - int(tiempo_actual_list[0])
-    min = int(tiempo_meta_list[1]) - int(tiempo_actual_list[1])
-    seg = int(tiempo_meta_list[2]) - int(tiempo_actual_list[2])
-
-    if hr > 0:
-        return print(f"{hr} : {min} : {seg}", end=" ")
-    elif min > 0:
-        return print(f"{min} : {seg}", end=" ")
-    elif seg > 0:
-        return print(f"{seg}", end=" ")
-    else:
-        return print("Se acabo el tiempo")
-
-def temporizador(minutos, mensaje_a_mostrar):
-    segundos = minutos * 60
-    tiempo_meta = time.time() + segundos
-    limpiar_pantalla()
-    while time.time() != tiempo_meta:
-        print(f"{mensaje_a_mostrar}\tTiempo restante: {time.strftime("%M : %S", time.localtime(time.time() - tiempo_meta))}")
-    limpiar_pantalla()
-
-verificar_ingreso_tiempo = False
+def temporizador(duracion, etapa):
+    duracion *= 60
+    for restante in range(duracion, -1, -1):
+        print("\r")
+        hr, resto = divmod(restante, 3600)
+        min, seg = divmod(resto, 60)
+        print("Tiempo restante de {} {:0>2}:{:0>2}:{:0>2}".format(etapa.lower(), hr, min, seg))
+        time.sleep(1)
+        limpiar_pantalla()
 
 if bienvenida() == 1:
+
     limpiar_pantalla()
+
     tiempo_de_enfoque = validacion_int("Duracion de la fase de enfoque en minutos: ", "Caracter Invalido", 0)
     tiempo_de_descanso = validacion_int("Duracion de la fase de descanso en minutos: ", "Caracter Invalido", 0)
     numero_de_pomodoros = validacion_int("Ingrese el numero de etapas de concentracion que tendra el pomodoro: ", "Caracter Invalido", 0)
+
     print(f"Esta seguro de querer continua con sus {numero_de_pomodoros} Pomodoro(s) de {tiempo_de_enfoque} minuto(s) de enfoque y {tiempo_de_descanso} minuto(s) de descanso?: \n1)Si\n2)No")
+
     ciclos = [["Concentracion", tiempo_de_enfoque]]
+
     decision = validacion_int(":", "Caracter Invalido", 0, 2)
+
     for i in range(0, numero_de_pomodoros - 1):
         ciclos.append(["Descanso", tiempo_de_descanso])
         ciclos.append(["Concentracion", tiempo_de_enfoque])
+
     for fase in ciclos:
-            temporizador(fase[1], fase[0])
+        temporizador(fase[1], fase[0])
+        "Etapa terminada, pasando a la siguiente etapa."
+        subprocess.Popen(["~/sonido_de_pin.sh"], shell=True)
 else:
     pass
 
