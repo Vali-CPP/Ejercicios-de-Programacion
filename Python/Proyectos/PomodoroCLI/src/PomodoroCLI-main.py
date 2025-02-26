e879a4391edfdda2d2681e8d83df2ec7f80ba947
Valiant Molero <valiantmolero@gmail.com>
2025-02-26 09:49:41 -0500

    Se opto por aplicar POO para el manejo de los datos asi tenerlos de manera mas organizada, ademas de que se elimino la confirmacino del pomodoro bajo la logica de "Para todo lo que tienes que hacer para poder iniciar este Pomodoro, espero esten seguros, sino que lo inicien de nuevo y ya", ademas de que se cambio el script de finalizacion para una simple terminal que muestre el mensaje de "Tiempo terminado" y se pase automaticamente al descanso

--- Python/Proyectos/PomodoroCLI/src/PomodoroCLI-main.py
+++ Python/Proyectos/PomodoroCLI/src/PomodoroCLI-main.py
@@ -3,6 +3,37 @@ import sys
 import os
 import subprocess
 
+class Pomodoro:
+    def __init__(self, t_concentracion = 25, t_descanso = 5, n_ciclos = 3):
+        self.t_concentracion = t_concentracion
+        self.t_descanso = t_descanso
+        self.n_ciclos = n_ciclos
+
+    def definir_ciclos(self):
+
+        self.ciclos = list()
+
+        for i in range(0, self.n_ciclos):
+            self.ciclos.append(["Concentracion", t_concentracion])
+            self.ciclos.append(["Descanso", t_descanso])
+
+    def temporizador(self, etapa, duracion):
+
+        duracion *= 60
+
+        for restante in range(duracion, -1, -1):
+            limpiar_pantalla()
+            print("\r")
+            hr, resto = divmod(restante, 3600)
+            min, seg = divmod(resto, 60)
+            print("Tiempo restante de {} {:0>2}:{:0>2}:{:0>2}".format(etapa.lower(), hr, min, seg), end="")
+            time.sleep(1)
+            limpiar_pantalla()
+
+    def get_ciclos(self):
+        return self.ciclos
+
+
 def limpiar_pantalla():
     if os.name == "nt":
         os.system("cls")
@@ -25,38 +56,20 @@ def bienvenida():
     limpiar_pantalla()
     return validacion_int("Bienvenido a tu aplicacion de Pomodoro, dime que hacer: \n1)Iniciar un pomodoro\n2)Salir\n:", "Numero invalido", 0, 2)
 
-def temporizador(duracion, etapa):
-    duracion *= 60
-    for restante in range(duracion, -1, -1):
-        print("\r")
-        hr, resto = divmod(restante, 3600)
-        min, seg = divmod(resto, 60)
-        print("Tiempo restante de {} {:0>2}:{:0>2}:{:0>2}".format(etapa.lower(), hr, min, seg))
-        time.sleep(1)
-        limpiar_pantalla()
-
 if bienvenida() == 1:
 
     limpiar_pantalla()
 
-    tiempo_de_enfoque = validacion_int("Duracion de la fase de enfoque en minutos: ", "Caracter Invalido", 0)
-    tiempo_de_descanso = validacion_int("Duracion de la fase de descanso en minutos: ", "Caracter Invalido", 0)
-    numero_de_pomodoros = validacion_int("Ingrese el numero de etapas de concentracion que tendra el pomodoro: ", "Caracter Invalido", 0)
+    t_concentracion = validacion_int("Ingrese el tiempo de concentracion deseado:", "Tiempo invalido, debe ser mayor que cero", 0)
+    t_descanso = validacion_int("Ingrese el tiempo de descanso deseado:", "Tiempo invalido, debe ser mayor que cero", 0)
+    n_ciclos = validacion_int("Ingrese el numero de ciclos a completar:", "Tiempo invalido, el numero de ciclos tiene que ser mayor a cero", 0)
 
-    print(f"Esta seguro de querer continua con sus {numero_de_pomodoros} Pomodoro(s) de {tiempo_de_enfoque} minuto(s) de enfoque y {tiempo_de_descanso} minuto(s) de descanso?: \n1)Si\n2)No")
+    pomo_actual = Pomodoro(t_concentracion, t_descanso, n_ciclos)
+    pomo_actual.definir_ciclos()
 
-    ciclos = [["Concentracion", tiempo_de_enfoque]]
-
-    decision = validacion_int(":", "Caracter Invalido", 0, 2)
-
-    for i in range(0, numero_de_pomodoros - 1):
-        ciclos.append(["Descanso", tiempo_de_descanso])
-        ciclos.append(["Concentracion", tiempo_de_enfoque])
-
-    for fase in ciclos:
-        temporizador(fase[1], fase[0])
+    for fase in pomo_actual.get_ciclos():
+        pomo_actual.temporizador(fase[0], fase[1])
         "Etapa terminada, pasando a la siguiente etapa."
-        subprocess.Popen(["~/sonido_de_pin.sh"], shell=True)
+        subprocess.Popen(["~/notificacion_de_finalizacion.sh"], shell=True)
 else:
-    pass
-
+    sys.exit()
--- bash-scriptting/condicionales.sh
+++ bash-scriptting/condicionales.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 echo "Ingresa tu edad: "
 read edad
 
